b6aa1b3e0a732d672f5fc17e9ef634b85e2fba9d
Bart Fijneman <info@bartfijneman.com>
2022-05-19 00:56:19 +0200

    Update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => '865a626627b3a204a58db38fbacd995a7cb583e6',
         'name' => '__root__',
         'dev' => true,
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => '865a626627b3a204a58db38fbacd995a7cb583e6',
             'dev_requirement' => false,
         ),
         'guzzlehttp/guzzle' => array(
